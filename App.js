f4e14301309d2c7726a6918940a9575602e66c87
ajaybhatia <prof.ajaybhatia@gmail.com>
2020-03-05 13:06:55 +0530

    Added NetInfo package

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {View, StyleSheet} from 'react-native';
 import {RNCamera} from 'react-native-camera';
-import {View} from 'react-native';
+
+import NetInfo from '@react-native-community/netinfo';
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+  },
+});
 
 const App = () => {
+  React.useEffect(() => {
+    // Subscribe
+    const unsubscribe = NetInfo.addEventListener(state => {
+      console.log('Connection type', state.type);
+      console.log('Is connected?', state.isConnected);
+    });
+
+    // Unsubscribe
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <View>
+    <View style={styles.camera}>
       <RNCamera />
     </View>
   );
