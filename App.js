1136fdbf56884b5941acc3478aa88578b97d52a0
ajaybhatia <prof.ajaybhatia@gmail.com>
2020-03-05 12:49:56 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {RNCamera} from 'react-native-camera';
+import {View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <RNCamera />
+    </View>
+  );
+};
+
+export default App;
